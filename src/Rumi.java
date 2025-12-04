7e040803559aba5c13accaaa5898372277b0aa3e
vwaskey08-dotcom <vwaskey08@gmail.com>
2025-12-04 09:40:29 -0800

    Update Rumi.java

--- src/Rumi.java
+++ src/Rumi.java
@@ -5,7 +5,32 @@ public class Rumi {
     private int popularityLevel;
     private String hairColor;
 
-    public Rumi ()
+    public Rumi (String weapon, int energy, int popularityLevel, String hairColor)
+    {
+        this.weapon = weapon;
+        this.energy = energy;
+        this.popularityLevel = popularityLevel;
+        this.hairColor = hairColor;
+    }
+
+    public void eat() {
+        energy += 10;
+    }
+
+    public void sleep() {
+        energy += 20; 
+    }
+
+    public void fight(){
+        energy -= 15;
+    }
+
+    public void sing()
+    {
+        popularityLevel += 5;
+    }
+
+    public static void honmoonLevel()
     {
         
     }
