f713c5010dc9dc27eefcab4e8d8b1fa9fcd1b768
vwaskey08-dotcom <vwaskey08@gmail.com>
2025-12-04 09:28:14 -0800

    Add interface and rumi

---
+++ src/Rumi.java
@@ -0,0 +1,13 @@
+public class Rumi {
+    
+    private String weapon;
+    private int energy;
+    private int popularityLevel;
+    private String hairColor;
+
+    public Rumi ()
+    {
+        
+    }
+
+}
