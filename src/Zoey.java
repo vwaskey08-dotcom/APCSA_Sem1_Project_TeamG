e801baa04a5c459904dab45e4c2137d535c5a993
KimthN <nguyenthkim0407@gmail.com>
2025-12-11 09:28:01 -0800

    thursday fix class match class

--- src/Zoey.java
+++ src/Zoey.java
@@ -1,17 +1,68 @@
-public class Zoey {
+public class Zoey implements DemonHunter {
+    
     private String weapon;
     private int energy;
     private int popularityLevel;
     private String hairColor;
+    private String name;
 
     //constructor
-    public Zoey(String weapon, int energy, int popularityLevel, String hairColor){
-        this.weapon = weapon;
-        this.energy = energy;
-        this.popularityLevel = popularityLevel;
-        this.hairColor = hairColor;
+    public Zoey(){
+        this.weapon = "daggers";
+        this.energy = 50;
+        this.popularityLevel = 100;
+        this.hairColor = "black";
+        this.name = "Zoey";
     }
 
+    // Getters
+/**
+     * Getter for name
+     * @return name of character
+     */
+
+public String getName()
+{
+    return name;
+}
+
+/**
+ * Getter for weapon
+ * @return weapon of character
+ */
+public String getWeapon()
+{
+    return weapon;
+}
+
+/**
+ * Getter for energy
+ * @return energy of character
+ */
+public int getEnergy()
+{
+    return energy;
+}
+
+/**
+ * Getter for popularity level
+ * @return popularity level of character
+ */
+public int getPopularityLevel()
+{
+    return popularityLevel;
+}
+
+/**
+ * Getter for hair color
+ * @return hair color of character
+ */
+public String getHairColor()
+{
+    return hairColor;
+}
+
+// actions
     public void eat() {
         energy += 10;
     }
@@ -22,11 +73,18 @@ public class Zoey {
 
     public void write(){
         popularityLevel += 5;
+        energy -= 10;
     }
 
     public void fight(){
         energy -= 15;
+        popularityLevel -=10;
     }
-    
-    // this is too complicated
+
+    public String toString()
+    {
+        return "Character: " + name+ "\nWeapon: " + weapon + "\nEnergy: " + energy + "\nPopularity Level: " + popularityLevel + "\nHair Color: " + hairColor;
+    }
+
+
 }
