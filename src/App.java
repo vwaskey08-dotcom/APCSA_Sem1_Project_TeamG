a2309d2b7734bbcb632ff717209fe2799eb59ecb
KimthN <nguyenthkim0407@gmail.com>
2025-12-12 09:28:06 -0800

    random ending kim first draft

--- src/App.java
+++ src/App.java
@@ -81,8 +81,32 @@ public class App {
 while(character.energy < 100 && character.energy > 0){
     //something
 }
-//a way to win and die (ending) satisfy our random event here potentially 
+//a way to win and die (ending) satisfy our random event here potentially kim
+if (character.energy <= 0 || character.popularity <= 0){
+    int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
+    if (random_ending == 1){
+        System.out.println(character.name + " has died from exhaustion. Game Over.");
+    }
+    elif (random_ending == 2){
+        System.out.println("You failed to maintain" + character.name + "'s " + "popularity. The demons have broken the honmoon. Game Over.");
+    }
+    else {
+        System.out.println("The demons have taken over with their new hit song 'Hypnotic'. Game over.");
+    }
+}
 
+if (character.energy >= 100 || character.popularity >= 100){
+    int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
+    if (random_ending == 1){
+        System.out.println("Congratulations! You have successfully managed your K-pop demon hunter to fame and fortune!");
+    }
+    elif (random_ending == 2){
+        System.out.println("You have defeated the demons and restored peace to the honmoon! Victory is yours!");
+    }
+    else {
+        System.out.println(character.name + " has become the ultimate K-pop demon hunter, loved by all!");
+    }
+}
 //kim- if popularity level reaches a certain point you win, if energy level is below a certain point you die, random: random number generator, pick a number, after every while loop add +1 to random int, once it gets to random number do the event
 //kim random stuff- int randomEventCounter = 0; (int)(Math.random()*(5-1+1)+1), 
 
