6f1562e707e26a40c2221ddb4d7667d2a7f19a80
KimthN <nguyenthkim0407@gmail.com>
2025-12-15 09:50:39 -0800

    random comments

--- src/App.java
+++ src/App.java
@@ -157,12 +157,7 @@ public class App {
 
 
 
-
-
-
-
-
-// kim random ending
+// random ending, if the character loses energy popularity they randomly die from exhaustion, loss of popularity or demons take over with a new song
 if (character.energy <= 0 || character.popularity <= 0){
     int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
     if (random_ending == 1){
@@ -176,6 +171,7 @@ if (character.energy <= 0 || character.popularity <= 0){
     }
 }
 
+// random ending, if the character gains energy and popularity to the max they randomly win from fame and fortune, maintaining the honmoon  or becoming the ultimate k-pop demon hunter
 if (character.energy >= 100 || character.popularity >= 100){
     int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
     if (random_ending == 1){
@@ -213,32 +209,4 @@ if (character.energy >= 100 || character.popularity >= 100){
 //kim- if popularity level reaches a certain point you win, if energy level is below a certain point you die, random: random number generator, pick a number, after every while loop add +1 to random int, once it gets to random number do the event
 //kim random stuff- int randomEventCounter = 0; (int)(Math.random()*(5-1+1)+1), 
 >>>>>>> 5cca3776f26c5eb82741947d0bcfde20fdc95e65
-
-//if (character.energy <= 0 || character.popularity <= 0){
-    //int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
-    //if (random_ending == 1){
-        //System.out.println(character.name + " has died from exhaustion. Game Over.");
-    //}
-    //elif (random_ending == 2){
-        //System.out.println("You failed to maintain" + character.name + "'s " + "popularity. The demons have broken the honmoon. Game Over.");
-    //}
-    //else {
-        //System.out.println("The demons have taken over with their new hit song 'Hypnotic'. Game over.");
-    //}
-//}
-
-//if (character.energy >= 100 || character.popularity >= 100){
-    //int random_ending = (int)(Math.random() * (3 - 1 + 1) + 1);
-    //if (random_ending == 1){
-        //System.out.println("Congratulations! You have successfully managed your K-pop demon hunter to fame and fortune!");
-    //}
-    //elif (random_ending == 2){
-        //System.out.println("You have defeated the demons and restored peace to the honmoon! Victory is yours!");
-    //}
-    //else {
-        //System.out.println(character.name + " has become the ultimate K-pop demon hunter, loved by all!");
-    //}
-//}
-
-// random events
 
